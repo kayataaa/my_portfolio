7ae8a49e4eebfaad14b741c2f275746456a65f32
kayataaa <ayamashita124+124@gmail.com>
2020-04-10 16:47:27 +0900

    modify database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_04_09_053306) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "article_images", force: :cascade do |t|
     t.string "image_url"
     t.integer "article_id"
